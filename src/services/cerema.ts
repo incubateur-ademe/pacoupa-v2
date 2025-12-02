bca5e7eaf318831b6209054acbadc276a0c12223
Maxime Dréau <maxime@dreau.fr>
2025-12-02 11:14:40 +0100

    Revert "Améliore les perfs API Cerema"
    This reverts commit 596bedd1a85f0d3dc420ce404042b174427c0a0c.

--- src/services/cerema.ts
+++ src/services/cerema.ts
@@ -8,7 +8,7 @@ export async function getCeremaData(address: string): Promise<CeremaFeatureAttri
   try {
     const params = new URLSearchParams({
       f: "json",
-      where: `adresse = '${address}'`,
+      where: `UPPER(adresse) = UPPER('${address}')`,
       outFields: "*",
       returnGeometry: "true",
     });
